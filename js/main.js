4ada69c33297801bb951e529c4c5766371bf761c
munis <129666007+MuniS67@users.noreply.github.com>
2023-10-21 16:09:48 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+// ссыкла сервера
 let urlBase = "http://localhost:8080";
 
+// доставать элементы с верстки
 let tBody = document.querySelector("tbody");
 let btnAdd = document.querySelector(".title button");
 let modalWindow = document.querySelector(".modalMain");
@@ -25,14 +27,46 @@ let changeDescr = changeForma.querySelector("#descrChange");
 let changeDate = changeForma.querySelector("#dateChange");
 let changeTime = changeForma.querySelector("#timeChange");
 let changeDone = changeForma.querySelector("#doneChange");
-  
+
+let tableBottom = document.querySelector(".bottom");
+let cont = document.querySelector(".cont");
+cont.classList.add("hide");
+
+// функция для массива users
+function updateDataCard() {
+  fetch(urlBase + "/users")
+    .then((res) => res.json())
+    .then((res) => reloadCard(res, cont));
+}
+updateDataCard();
+
+// перевед на table и card
+let clickedITem = 0;
+midA.forEach((a, idx) => {
+  a.onclick = () => {
+    midA[clickedITem].classList.remove("active_a");
+    midA[idx].classList.add("active_a");
+    if (a.hasAttribute("data-table")) {
+      tableBottom.classList.remove("hide");
+      cont.classList.add("hide");
+      updateData();
+    } else {
+      tableBottom.classList.add("hide");
+      cont.classList.remove("hide");
+      updateDataCard();
+    }
+    clickedITem = idx;
+  };
+});
+// функция для массива users
 function updateData() {
   fetch(urlBase + "/users")
     .then((res) => res.json())
-    .then((res) => reload(res, tBody));
+    .then((res) => reloadTable(res, tBody));
 }
 updateData();
 
+// Форма добавления
 forma.onsubmit = (e) => {
   e.preventDefault();
 
@@ -53,6 +87,7 @@ forma.onsubmit = (e) => {
   }).then((res) => {
     if (res.status === 200 || res.status === 201) {
       updateData();
+      updateDataCard();
     }
   });
 
@@ -67,10 +102,89 @@ forma.onsubmit = (e) => {
 btnAdd.onclick = (e) => {
   e.preventDefault();
   modalWindow.classList.remove("hide");
+  modalContent.classList.remove("modalClose");
   modalContent.classList.add("modalAnim");
 };
+// функция для card
+function reloadCard(arr, place) {
+  place.innerHTML = "";
+  for (let item of arr) {
+    let cardDiv = document.createElement("div");
+    let cardTitle = document.createElement("h2");
+    let cardDescr = document.createElement("p");
+    let cardInf = document.createElement("div");
+    let cardInfDate = document.createElement("p");
+    let cardInfTime = document.createElement("p");
+    let cardDone = document.createElement("p");
 
-function reload(arr, place) {
+    cardDiv.classList.add("card_div");
+    cardTitle.classList.add("card_title");
+    cardDescr.classList.add("card_p");
+    cardInf.classList.add("card_inf");
+    cardDone.classList.add("card_p");
+
+    cardTitle.innerHTML = item.taskTitle;
+    cardDescr.innerHTML = item.description;
+    cardInfDate.innerHTML = item.date;
+    cardInfTime.innerHTML = item.time;
+    cardDone.innerHTML = item.done;
+
+    cont.append(cardDiv);
+    cardDiv.append(cardTitle, cardDescr, cardInf, cardDone);
+    cardInf.append(cardInfDate, cardInfTime);
+
+    cardDiv.ondblclick = () => {
+      modalWindowChange.classList.remove("hide");
+      changeTitle.value = item.taskTitle;
+      changeDescr.value = item.description;
+      changeDate.value = item.date;
+      changeTime.value = item.time;
+      changeDone.value = item.done;
+
+      changeForma.onsubmit = (e) => {
+        e.preventDefault();
+
+        item.taskTitle = changeTitle.value;
+        item.description = changeDescr.value;
+        item.date = changeDate.value;
+        item.time = changeTime.value;
+        item.done = changeDone.value;
+
+        fetch(urlBase + "/users/" + item.id, {
+          method: "put",
+          body: JSON.stringify({
+            taskTitle: changeTitle.value,
+            description: changeDescr.value,
+            date: changeDate.value,
+            time: changeTime.value,
+            done: changeDone.value,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 200 || res.status === 201) {
+            updateDataCard();
+            modalWindowChange.classList.add("hide");
+            forma.reset();
+          }
+        });
+      };
+      deleteBtn.onclick = () => {
+        fetch(urlBase + "/users/" + item.id, {
+          method: "delete",
+        }).then((res) => {
+          if (res.status === 200 || res.status === 201) {
+            cardDiv.remove();
+            modalWindowChange.classList.add("hide");
+          }
+        });
+      };
+    };
+  }
+}
+// функция для table
+function reloadTable(arr, place) {
   place.innerHTML = "";
   for (let item of arr) {
     let tr = document.createElement("tr");
@@ -128,13 +242,13 @@ function reload(arr, place) {
       };
       deleteBtn.onclick = () => {
         fetch(urlBase + "/users/" + item.id, {
-          method: "delete"
+          method: "delete",
         }).then((res) => {
-          if(res.status === 200 || res.status === 201) {
-            tr.remove()
+          if (res.status === 200 || res.status === 201) {
+            tr.remove();
             modalWindowChange.classList.add("hide");
           }
-        })
+        });
       };
     };
   }
